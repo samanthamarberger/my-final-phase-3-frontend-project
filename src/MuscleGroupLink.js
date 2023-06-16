db6ae4022cd81abb27b3fa7ca3adc199d9a183c7
samanthamarberger <slatham04@gmail.com>
2023-06-16 15:48:00 -0600

    added link content

--- src/MuscleGroupLink.js
+++ src/MuscleGroupLink.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function MuscleGroupLink({ muscleGroup }) {
 
+    return(
+        <div>
+            <Link to={`http://localhost:9292/muscle-groups/${muscleGroup.id}`} >
+                <h3>{muscleGroup.name}</h3>
+            </Link>
+        </div>
+    )
 }
 
 export default MuscleGroupLink;
