2b4a3f2ded59e091cfa8b8ec155fc30ed391d17f
samanthamarberger <slatham04@gmail.com>
2023-06-16 15:24:51 -0600

    removed unnecessary components from template and added fetch request for muscle groups

--- src/MuscleGroups.js
+++ src/MuscleGroups.js
@@ -4,6 +4,12 @@ import React, { useState, useEffect } from "react";
 function MuscleGroups() {
    const [muscleGroups, setMuscleGroups] = useState([]);
    const [muscleGroupFormFlag, setMuscleGroupFormFlag] = useState(false);
+
+   useEffect(() => {
+    fetch('http://localhost:9292/muscle_groups')
+    .then ((r) => r.json())
+    .then ((musclegroups) => setMuscleGroups(musclegroups))
+   }, [])
     return (
         <div>
             <h1> Hi </h1>
