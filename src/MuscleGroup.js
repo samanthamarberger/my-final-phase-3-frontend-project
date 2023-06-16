94e5c4a8cc637d050485227597628fba82772af8
samanthamarberger <slatham04@gmail.com>
2023-06-16 15:17:37 -0600

    added content to muscle group component

---
+++ src/MuscleGroup.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import MuscleGroupLink from "./MuscleGroupLink";
+
+function MuscleGroups() {
+   const [muscleGroups, setMuscleGroups] = useState([]);
+   const [muscleGroupFormFlag, setMuscleGroupFormFlag] = useState(false);
+    return (
+        <div>
+            <h1> Hi </h1>
+        </div>
+    )
+
+}
+
+export default MuscleGroups;
