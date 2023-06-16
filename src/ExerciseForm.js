5d5c399e7304a316dc07ba879bb66cade076fafa
samanthamarberger <slatham04@gmail.com>
2023-06-16 16:51:10 -0600

    Added add exercise form

---
+++ src/ExerciseForm.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function ExerciseForm() {
+    const [name, setName] = useState("");
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default ExerciseForm;
