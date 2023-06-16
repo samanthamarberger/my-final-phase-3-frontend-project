baa81479325de1369854304c2b8c3ed9f6ff540e
samanthamarberger <slatham04@gmail.com>
2023-06-16 16:47:53 -0600

    added exercises

--- src/Exercise.js
+++ src/Exercise.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Exercise(){
+function Exercise({ exercise }){
 
+return(
+    <div>
+        <h3>{exercise.name}</h3>
+    </div>
+)
 }
 
 export default Exercise;
--- src/MuscleGroup.js
+++ src/MuscleGroup.js
@@ -1,16 +1,33 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Exercise from "./Exercise";
 
-function MuscleGroup() {
+function MuscleGroup(props) {
     const [muscleGroup, setMuscleGroup] = useState({
         exercises: []
     });
 
     const [exerciseFormFlag, setExerciseFormFlag] = useState(false);
 
+    const params = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:9292/muscle_groups/${params.id}`)
+        .then((r) => r.json())
+        .then ((muscleGroup) => setMuscleGroup(muscleGroup))
+    }, [])
+
+    const exercises = muscleGroup.exercises.map((exercise) => <Exercise key={exercise.id} exercise={exercise} />)
+
     return (
         <div>
-            <h1>muscle group</h1>
+            <br />
+            <h2>{muscleGroup.name}</h2>
+            <hr/>
+            <h3>Exercises:</h3>
+            <br />
+            {exercises}
+            <br />
         </div>
     )
 }
