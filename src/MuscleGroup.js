acc92eaa55537f19c300990306e427a9b41b33b0
samanthamarberger <slatham04@gmail.com>
2023-06-16 15:54:09 -0600

    fixed the link to the correct link

--- src/MuscleGroup.js
+++ src/MuscleGroup.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function MuscleGroup() {
-
+    return (
+        <div>
+            <h1>muscle group</h1>
+        </div>
+    )
 }
 
 export default MuscleGroup;
--- src/MuscleGroupLink.js
+++ src/MuscleGroupLink.js
@@ -5,7 +5,7 @@ function MuscleGroupLink({ muscleGroup }) {
 
     return(
         <div>
-            <Link to={`http://localhost:9292/muscle-groups/${muscleGroup.id}`} >
+            <Link to={`http://localhost:3000/muscle-groups/${muscleGroup.id}`} >
                 <h3>{muscleGroup.name}</h3>
             </Link>
         </div>
