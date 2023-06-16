683d1d7ebcabddf824946bd4e1cc8a1cc17cfd47
samanthamarberger <slatham04@gmail.com>
2023-06-16 15:16:52 -0600

    added contents to home page

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+    <div className="App">
+        <header className="App-header">
+            <h3>Your Body, Your Workouts!</h3>
+        </header>
+    </div>
+    )
+}
+
+export default Home
