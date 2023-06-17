b1f7f4968bef4db988eadfde845f18b9ffc5f651
samanthamarberger <slatham04@gmail.com>
2023-06-17 17:09:28 -0600

    Added value to the form

--- src/ExerciseForm.js
+++ src/ExerciseForm.js
@@ -7,13 +7,27 @@ function ExerciseForm() {
     const [muscleGroupId, setMuscleGroupId] = useState("");
 
     return(
-        <form>
-            <label>New Exercise: </label>
-            <input type="text" name="Name"/>
-            <input type="text" name="Image URL"/>
-            <input type="text" name="How To Do"/>
-            <input type="submit"/>
-        </form>
+        <div className="form">
+            <form>
+                <label>Add a new exercise</label>
+                <label>
+                    Name: 
+                    <input type="text" name="Name" value={name}/>
+                </label>
+                <br />
+                <label>
+                    Image URL:
+                    <input type="text" name="Image URL" value={image}/>
+                </label>
+                <br />
+                <label>
+                    How To Do:
+                    <input type="text" name="How To Do" value={howTo}/>
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     )
 }
 
