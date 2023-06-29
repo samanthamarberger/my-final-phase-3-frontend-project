6c18c3ee89fb563b874f9ceb6614e41516a0b6c0
samanthamarberger <slatham04@gmail.com>
2023-06-29 16:46:40 -0600

    Added pictures to the home page

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,17 @@ function Home() {
             various workouts to those who are beginning their 
             fitness journey. Within this application you can look 
             at workouts, as well as how to do them, based on the 
-            muscle group you are targeting. </p>
+            muscle group you are targeting. 
+        </p>
+        <img src="https://hips.hearstapps.com/hmg-prod/images/trek-domane-mk4-4-1662643874.jpg"
+            alt="Bike Image"
+        />
+        <img src="https://cdn.shoplightspeed.com/shops/643683/files/45605395/best-exercise-equipment-for-building-muscle.jpg"
+            alt="gym equipment"
+        />
+        <img src="https://taskandpurpose.com/uploads/2021/04/15/Womens-Running-Shoes-Featured.jpg?auto=webp"
+            alt="Running shoes"
+        />
     </div>
     )
 }
